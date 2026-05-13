13e2532b7f0fd6584e152ae7ec6fbb5b4065e14a
Amirhassan5303 <amirhassan5303@gmail.com>
2026-05-13 10:55:52 +0200

    Implement flashcard slider UI

--- script.js
+++ script.js
@@ -1,4 +1,32 @@
-function flipCard() {
-  const card = document.getElementById("card");
-  card.classList.toggle("flipped");
+const track = document.getElementById("track");
+const cards = document.querySelectorAll(".card");
+const prevBtn = document.querySelector(".prev");
+const nextBtn = document.querySelector(".next");
+
+let currentIndex = 0;
+
+function updateSlider() {
+  const cardWidth = cards[0].offsetWidth;
+  track.style.transform = `translateX(-${currentIndex * cardWidth}px)`;
 }
+
+function flipCard(button) {
+  const flashcard = button.closest(".flashcard");
+  const cardInner = flashcard.querySelector(".flashcard-inner");
+  cardInner.classList.toggle("flipped");
+}
+
+nextBtn.addEventListener("click", () => {
+  if (currentIndex < cards.length - 1) {
+    currentIndex++;
+    updateSlider();
+    console.log(currentIndex);
+  }
+});
+
+prevBtn.addEventListener("click", () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+    updateSlider();
+  }
+});
